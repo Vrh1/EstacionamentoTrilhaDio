ea6adbb6a6ac26bf8e365fb508d6b2559f183a9d
Vergara <jvv10.vergara@gmail.com>
2022-09-13 09:45:26 -0300

    Initial commit

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamentoTrilhaDio.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial,decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar: ");
+            string placa = Console.ReadLine();
+            if (!string.IsNullOrEmpty(placa))
+            {
+                veiculos.Add(placa);
+            }
+            else
+            {
+                Console.WriteLine("Valor da placa vazio.");
+                AdicionarVeiculo();
+            }
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover: ");
+            foreach (string vagas in veiculos)
+            {
+                Console.WriteLine($"Veículo - {vagas}");
+            }
+            string placa = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(placa) && veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
+                int horas = 0;
+                decimal valorTotal = 0;
+                horas = Convert.ToInt32(Console.ReadLine());
+                valorTotal = (precoInicial + precoPorHora * horas);
+
+                veiculos.Remove(placa);
+
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Valor da placa vazio.");
+                RemoverVeiculo();
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são: ");
+                foreach(string vagas in veiculos)
+                {
+                    Console.WriteLine(vagas);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados. ");
+            }
+        }
+
+    }
+
+
+}
